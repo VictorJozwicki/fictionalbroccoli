adc85869c77832add4483590b909f266ddae09d2
Victor Jozwicki <victor.jozwicki@gmail.com>
2019-02-04 14:30:59 +0100

    beginning of list

--- fictionalbroccoli/fictionalbroccoli/ViewModels/BrocoRegisterViewModel.cs
+++ fictionalbroccoli/fictionalbroccoli/ViewModels/BrocoRegisterViewModel.cs
@@ -6,15 +6,31 @@ using Prism.Commands;
 using Prism.Navigation;
 using Prism.Logging;
 using Prism.Services;
+using fictionalbroccoli.Models;
+using fictionalbroccoli.Services;
 
 namespace fictionalbroccoli.ViewModels
 {
     public class BrocoRegisterViewModel : ViewModelBase
     {
+        //private IRegisterService _registerService;
+        //private List<Registration> _registrations;
+        //public List<Registration> Registrations
+        //{
+        //    get { return _registrations;  }
+        //    set { SetProperty(ref _registrations, value);  }
+        //}
+
         public BrocoRegisterViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "Enregistrements";
+            Registration temp = new Registration("title", "description");
 
+            //_registerService = RegisterService;
+
+            //_registerService.Add(temp);
+
+            //_registrations = _registerService.GetAll();
         }
     }
 }
