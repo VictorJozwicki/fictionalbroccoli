e0b00386911543e03beb970db54d248b0df6853d
Victor Jozwicki <victor.jozwicki@gmail.com>
2019-02-14 18:27:23 +0100

    Can almost sort

--- fictionalbroccoli/fictionalbroccoli/ViewModels/BrocoRegisterViewModel.cs
+++ fictionalbroccoli/fictionalbroccoli/ViewModels/BrocoRegisterViewModel.cs
@@ -18,41 +18,57 @@ namespace fictionalbroccoli.ViewModels
         public DelegateCommand<Registration> CommandGoDetail { get; private set; }
         public DelegateCommand<Registration> CommandSortUp { get; private set; }
         public DelegateCommand<Registration> CommandSortDown { get; private set; }
-        private List<Registration> _registrations;
-        private string _arrowUp = "arrowUpSelected", _arrowDown = "arrowDown";
+        public DelegateCommand<Registration> CommandSort { get; private set; }
 
+        private List<Registration> _registrations;
         public List<Registration> Registrations
         {
             get { return _registrations;  }
             set { SetProperty(ref _registrations, value);  }
         }
 
-        public string ArrowUp
+        // Sorting
+        private string _sortText = "Plus récent", _sortImageSource = "lightUpArrow";
+        public string SortText
         {
-            get { return _arrowUp; }
-            set { SetProperty(ref _arrowUp, value); }
+            get { return _sortText; }
+            set { SetProperty(ref _sortText, value); }
         }
 
-        public string ArrowDown
+        public string SortImageSource
         {
-            get { return _arrowDown; }
-            set { SetProperty(ref _arrowDown, value); }
+            get { return _sortImageSource; }
+            set { SetProperty(ref _sortImageSource, value); }
         }
 
+        private bool _sortRecent = true;
+        public bool SortRecent
+        {
+            get { return _sortRecent; }
+            set { SetProperty(ref _sortRecent, value); }
+        }
+
+        // Functions
         public BrocoRegisterViewModel(INavigationService navigationService, IRegisterService registerService) : base(navigationService)
         {
             Title = "Enregistrements";
             CommandGoDetail = new DelegateCommand<Registration>(HandleDetail);
-            CommandSortUp = new DelegateCommand<Registration>(HandleSortUp);
-            CommandSortDown = new DelegateCommand<Registration>(HandleSortDown);
+            CommandSort = new DelegateCommand<Registration>(HandleSort);
+
 
 
             _navigationService = navigationService;
             _registerService = registerService; // Local service
-            Registrations = _registerService.GetAll(); // Gotta catch them all
-
         }
 
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            Registrations = _registerService.GetAll(); // Gotta catch them all
+            this.SortUp();
+        }
+
+
+        // Handlers
         private void HandleDetail(Registration selectedRegistration)
         {
             var navigationParam = new NavigationParameters();
@@ -60,24 +76,33 @@ namespace fictionalbroccoli.ViewModels
             _navigationService.NavigateAsync("BrocoRegisterDetail", navigationParam);
         }
 
-        private void HandleSortUp(Registration obj)
+        private void HandleSort(Registration obj)
         {
-            ArrowUp = "arrowUpSelected";
-            ArrowDown = "arrowDown";
-            // TODO Sort up
-
+            if(SortRecent)
+            {
+                SortRecent = false;
+                SortText = "Plus ancien";
+                SortImageSource = "lightDownArrow";
+                Registrations = SortDown();
+            }
+            else
+            {
+                SortRecent = true;
+                SortText = "Plus récent";
+                SortImageSource = "lightUpArrow";
+                SortUp();
+            }
         }
 
-        private void HandleSortDown(Registration obj)
+        private void SortUp()
         {
-            ArrowUp = "arrowUp";
-            ArrowDown = "arrowDownSelected";
-            // TODO Sort down
+            Registrations.Sort((x, y) => DateTime.Compare(y.Date, x.Date)); // Fonctionne tout seul
         }
 
-        public override void OnNavigatedTo(INavigationParameters parameters)
+        private List<Registration> SortDown()
         {
-            Registrations = _registerService.GetAll(); // Gotta catch them all
+            Registrations.Sort((x, y) => DateTime.Compare(x.Date, y.Date)); // Fonctionne tout seul
+            return Registrations;
         }
     }
 }
