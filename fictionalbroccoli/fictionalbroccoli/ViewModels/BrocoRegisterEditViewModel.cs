6d4ab43ceca932730373fd5959ed10fc3df51bc5
Thibert Letullier <thibert57@me.com>
2019-02-26 13:59:12 +0100

    On peut maintenant éditer la photo - Il faudrait trouver un moyen de regrouper le code (comme dans des directives/composants angular)

--- fictionalbroccoli/fictionalbroccoli/ViewModels/BrocoRegisterEditViewModel.cs
+++ fictionalbroccoli/fictionalbroccoli/ViewModels/BrocoRegisterEditViewModel.cs
@@ -3,6 +3,8 @@ using Prism.Navigation;
 using fictionalbroccoli.Models;
 using fictionalbroccoli.Services;
 using System.Diagnostics;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
 
 namespace fictionalbroccoli.ViewModels
 {
@@ -10,6 +12,7 @@ namespace fictionalbroccoli.ViewModels
     {
         public INavigationService _navigationService;
         public IRegisterService _registerService;
+        public IPictureService _pictureService;
 
         public Registration _registration;
         public Registration Registration
@@ -18,23 +21,42 @@ namespace fictionalbroccoli.ViewModels
             set { SetProperty(ref _registration, value); }
         }
 
-        public DelegateCommand CommandSave { get; private set; }
+        public ImageSource _imageSrc;
+        public ImageSource ImageSrc
+        {
+            get { return _imageSrc; }
+            set { SetProperty(ref _imageSrc, value); }
+        }
 
-        public BrocoRegisterEditViewModel(INavigationService navigationService, IRegisterService registerService) : base(navigationService)
+        public DelegateCommand CommandUpdate { get; private set; }
+        public DelegateCommand CommandTakePicture { get; private set; }
+
+        public BrocoRegisterEditViewModel(INavigationService navigationService, IRegisterService registerService, IPictureService pictureService) : base(navigationService)
         {
             Title = "Edit Register";
             _navigationService = navigationService;
             _registerService = registerService;
-            CommandSave = new DelegateCommand(HandleSave);
+            _pictureService = pictureService;
+            CommandTakePicture = new DelegateCommand(HandleTakePicture);
+            CommandUpdate = new DelegateCommand(HandleUpdate);
         }
 
         public override void OnNavigatingTo(INavigationParameters parameters)
         {
             base.OnNavigatingTo(parameters);
             Registration = parameters.GetValue<Models.Registration>("Registration");
+            ImageSrc = ImageSource.FromFile(Registration.ImagePath);
         }
 
-        private void HandleSave()
+        private async void HandleTakePicture()
+        {
+            Debug.WriteLine("ok");
+            MediaFile file = await _pictureService.TakeFromCamera();
+            ImageSrc = ImageSource.FromStream(file.GetStream);
+            Registration.ImagePath = file.Path;
+        }
+
+        private void HandleUpdate()
         {
             _registerService.Update(Registration);
             var navigationParam = new NavigationParameters();
